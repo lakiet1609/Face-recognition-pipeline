8c845509fc362166742be20cc1fa84a8fe4748e3
lakiet1609 <kiettuanminhla1609@gmail.com>
2023-09-05 11:24:34 +0000

    update base database completed

--- src/database/base_database.py
+++ src/database/base_database.py
@@ -5,4 +5,20 @@ from src.utils.logger import Logger
 class BaseDatabase(object):
     def __init__(self, config):
         self.hostname = config['hostname']
+        self.port = config['port']
+        self.user = config['user']
+        self.password = config['password']
+        if (self.user == None or self.password == None) or (self.user == '' or self.password == ''):
+            self.url = f'mongodb://{self.hostname}:{self.port}'
+        else:
+            self.url = f"mongodb://{self.user}:{self.password}@{self.hostname}:{self.port}"
         
+        self.logger = Logger.__call__().get_logger()
+        self.initialize()
+    
+    def initialize(self):
+        try:
+            self.client = pymongo.MongoClient(self.url)
+            self.logger.info('Connected to MongoDB successfully !')
+        except pymongo.errors.ServerSelectionTimeoutError as er:
+            self.logger.info(f'Cannot connect to MongoDB: {er}')
