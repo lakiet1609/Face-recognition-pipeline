ab28dc276c1053bfebdc23aafe068f252e00330f
lakiet1609 <kiettuanminhla1609@gmail.com>
2023-09-05 11:14:18 +0000

    update database config

--- src/database/base_database.py
+++ src/database/base_database.py
@@ -1,3 +1,7 @@
 import pymongo
 from datetime import datetime
 from src.utils.logger import Logger
+
+class BaseDatabase(object):
+    def __init__(self, config):
+        self.hostname = config['hostname']
