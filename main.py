c3b89b2f8ca6e7990b7d55e98f8d8154266eaebb
lakiet1609 <kiettuanminhla1609@gmail.com>
2023-09-05 10:26:02 +0000

    face recognition app completed

---
+++ main.py
@@ -0,0 +1,7 @@
+from src.models.face_recognition import FaceRecognition
+import cv2
+
+img = cv2.imread('test/ronaldo1.jpg') 
+
+detection = FaceRecognition().get_face_encode(img=img, largest_box=True)
+print(detection)
