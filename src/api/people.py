7adb7d6c7056298f44f4df765a6758f15742425a
lakiet1609 <kiettuanminhla1609@gmail.com>
2023-09-06 09:12:21 +0000

    api for people file completed

--- src/api/people.py
+++ src/api/people.py
@@ -17,3 +17,21 @@ async def select_person_by_ID(person_id: str):
     person_doc = person_crud.select_person_by_id(person_id)
     return person_doc
 
+@router.post('', status_code= status.HTTP_201_CREATED)
+async def insert_one_person(person_id, name):
+    person_id, name = unquote(person_id), unquote(name)
+    person_doc = person_crud.insert_person(person_id, name)
+    return person_doc
+
+@router.post('/{person_id}/name', response_class= Response, status_code= status.HTTP_204_NO_CONTENT)
+async def update_name(person_id: str, name: str):
+    person_id, name = unquote(person_id), unquote(name)
+    person_crud.update_person_name(person_id, name)
+
+@router.delete('/{id}', response_class= Response, status_code= status.HTTP_204_NO_CONTENT)
+async def delete_person_by_ID(id: str):
+    person_crud.delete_person_by_id(id)
+
+@router.delete('', response_class= Response, status_code= status.HTTP_204_NO_CONTENT)
+async def delete_all_people():
+    person_crud.delete_all_people()
