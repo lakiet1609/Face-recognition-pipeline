ffdfd723074753d3f8529e18a4f64e6936d45ad1
lakiet1609 <kiettuanminhla1609@gmail.com>
2023-09-14 14:08:54 +0000

    update tensorrt for scrfd components

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
-from src.pipeline.face_recognition import FaceRecognition
+from src.components.tensorrt.scrfd_trt import SCRFD_TRT
+from src.components.scrfd import SCRFD
+from src.utils.common import read_yaml
 import cv2
 
-face_recognition = FaceRecognition()
+img = cv2.imread('faces/CR7/bd10d9ec07894146bc8884bc161de5c8.jpg')
 
-img = cv2.imread('test/ronaldo1.jpg')
-
-result = face_recognition.get_recognize(img)
-print(result)
-print(type(result[0]['recognition']['score']))
-
+config = read_yaml('configs/scrfd_trt_cfg.yaml')
+scrfd_trt = SCRFD_TRT(config)
+scrfd_trt.detect(img)
 
 
 
