3c087318f4293be86052930d385897abc4062a4f
lakiet1609 <kiettuanminhla1609@gmail.com>
2023-09-14 14:32:46 +0000

    update config file and configuration master file

--- src/config/configuration.py
+++ src/config/configuration.py
@@ -1,4 +1,5 @@
 from src.utils.common import Singleton, read_yaml
+from src.utils.logger import Logger
 
 class Configuration(object, metaclass=Singleton):
     def __init__(self):
@@ -7,13 +8,25 @@ class Configuration(object, metaclass=Singleton):
         self.face_embedding = None
         self.faiss = None
         self.database = None
+        self.logger = Logger.__call__().get_logger()
         self.init_config()
     
     def init_config(self):
         config = read_yaml(self.config)
-        self.face_detection = read_yaml(config['face_detection'])
+
+        face_detection_config = config['face_detection']
+        self.used_engine = face_detection_config['used_engine']
+
+        if self.used_engine in face_detection_config.keys():
+            self.face_detection = read_yaml(config[self.used_engine])
+        else:
+            self.logger.error(f'Does not support this engine {self.used_engine}')
+            exit()
+
         self.face_embedding = read_yaml(config['face_embedding'])
+
         self.faiss = read_yaml(config['faiss'])
+
         self.database = read_yaml(config['database'])
     
     def get_face_detection(self):
